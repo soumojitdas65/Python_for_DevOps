099f662bfa9542151ab5000599106f19f0334798
Soham Gupta <97831613+gupta-soham@users.noreply.github.com>
2022-08-09 22:34:43 +0530

    Added some additions and made minor changes

--- CalculatorFour.py
+++ CalculatorFour.py
@@ -11,32 +11,38 @@ print("6. Remainder (%)")
 
 
 while True:
-    n1 = input("Enter your 1st Number: ")                                   # User provides the 1st Number as Input (Integer or Float)
-    n2 = input("Enter your 2nd Number: ")                                   # User provides the 2nd Number as Input (Integer or Float)
-    option = int(input("Enter your requested Operator: "))                  # Lets User enter a valid Operator from above-mentioned list.
+# User provides the 2nd Number as Input (Integer or Float)
+    option = input("Enter your requested Operator:\n")                  # Lets User enter a valid Operator from above-mentioned list.
 
-    if option not in ("1", "2", "3", "4", "5", "6"):
-        print("Invalid Option for Operator")
-    else:
-        if      option == "1":
-                    print(n1, "+", n2,"=", n1+n2)
-        elif    option == "2":
-                    print(n1, "-", n2,"=", n1-n2)
-        elif    option == "3":
-                    print(n1, "*", n2,"=", n1*n2)
-        elif    option == "4":
-                    print(n1, "/", n2,"=", n1/n2)
-        elif    option == "5":
-                    print(n1, "^", n2,"=", n1**n2)
-        elif    option == "6":
-                    print(n1, "//", n2,"=", n1%n2)
+    if option in ("1", "2", "3", "4", "5", "6"):
+        n1 = float(input("Enter your 1st Number: "))                                   # User provides the 1st Number as Input (Integer or Float)
+        n2 = float(input("Enter your 2nd Number: "))  
+    
+    
+        if option == "1":
+            print(n1, "+", n2,"=", n1+n2)
+            
+        elif option == "2":
+            print(n1, "-", n2,"=", n1-n2)
+            
+        elif option == "3":
+            print(n1, "*", n2,"=", n1*n2)
+            
+        elif option == "4":
+            print(n1, "/", n2,"=", n1/n2)
+            
+        elif option == "5":
+            print(n1, "^", n2,"=", n1**n2)
+            
+        elif option == "6":
+            print(n1, "//", n2,"=", n1%n2)
+       
 
 # This part of code lets User decide if the person wants to end the Calculator or still continue....                  
-    print("Do you want to Continue ? (Y/N)")
-    response = str(input(("Your Response: ")))
+        print("Do you want to Continue ? (Y/N)")
+        response = str(input(("Your Response: ")))
 
-    if response != "N":
-        continue
+        if response == "N":
+            break
     else:
-        print("The Program Ends here, Goodbye")
-        break
+        print("Invalid Option for Operator")
