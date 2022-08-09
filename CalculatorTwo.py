2d4c6a17f3213e41f232c3d0de2a4aa5824acd76
soumojitdas65 <67901836+soumojitdas65@users.noreply.github.com>
2022-08-09 21:28:32 +0530

    Create CalculatorTwo.py
    Updated Version of Calculator which operated all the arithmetic operations at once.

---
+++ CalculatorTwo.py
@@ -0,0 +1,11 @@
+# This version of Calculator prints all the Operations done on the Inputs provided by User.
+
+Num1 = float(input("Enter First Number: "))
+Num2 = float(input("Enter Second Number: "))
+
+# This is Calculatortwo_v1.0 (Use by removing "#") which computes the result directly into "FLOAT" values
+       print("Addition of Num1 & Num2: ", Num1+Num2)
+       print("Subtraction of Num1 & Num2: ", Num1-Num2)
+       print("Multiplication of Num1 & Num2: ", Num1*Num2)
+       print("Division of Num1 & Num2: ", Num1/Num2)
+       print("Num1 to the power of Num2: ", Num1**Num2)
