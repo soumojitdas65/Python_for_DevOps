5f9c219282df19ee3f1ebd8d4a3b48cfe0ee4ce0
soumojitdas65 <67901836+soumojitdas65@users.noreply.github.com>
2022-08-09 21:23:21 +0530

    CalculatorOne.py

--- Calculator1.py
+++ Calculator1.py
@@ -1,3 +1,5 @@
+# This is my version of Calculator whose logic is based upon my COBOL Experience.
+
 A = int(input("Enter 1st Number:"))
 print(A)
 B = int(input("Enter 2nd Number:"))
@@ -5,6 +7,7 @@ print(B)
 C = str(input("Operation:"))
 print(C)
 
+# This Calculator uses "IF/ ELIF/ ELSE" functions
 if   C == "ADD":
     print("Result: ", A+B)
 elif C == "SUB":
