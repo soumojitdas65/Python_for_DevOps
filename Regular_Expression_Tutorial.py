7c860bac8781fdd24483f70f973456a83a8fe251
Soumojit Das <67901836+soumojitdas65@users.noreply.github.com>
2022-08-13 17:49:31 +0530

    Create Regular_Expression_Tutorial.py
    Tutorial on Regular Expressions.

---
+++ Regular_Expression_Tutorial.py
@@ -0,0 +1,13 @@
+# This is Regular Expression Tutorial Basic Code.
+
+"""
+Regular Expression for extracting e-mail IDs.
+"""
+
+import re                                       # To import "Regular Expression" Library.
+
+txt = """This text service notifies soumojitdas65@gmail.com and soumojitdas65@hotmail.com and soumojitdas65@yahoo.com"""
+
+mails = re.findall("[a-z0-9\.\-\_]+@+[a-z0-9\.\-\_]+.+[a-z]", txt)
+
+print(mails)
